4c4cc01e78539ea9f81055d960ed150109ef6937
cs1086 <xtnact541@gmail.com>
2022-06-07 10:50:03 +0800

    v2.0.2 adjust controller variable to obs and it can be observe by obx(()=>Text())

--- lib/my_widget.dart
+++ lib/my_widget.dart
@@ -6,9 +6,12 @@ class MyWidget extends StatelessWidget {
   final CountController ctrl = Get.find();
   @override
   Widget build(BuildContext context) {
-    return Container(child: GetBuilder<CountController>(
-        builder: (_) => Text(
-          'clicks: ${ctrl.count}',
-        )),);
+    // return Container(child: GetBuilder<CountController>(
+    //     builder: (_) => Text(
+    //       'clicks: ${ctrl.count}',
+    //     )),);
+    return Container(child: Obx(()=>Text(
+      'clicks: ${ctrl.count}',
+    ),),);
   }
 }
