f08db7f6bd92f66df19014a5f705836e6e8ac7df
cs1086 <xtnact541@gmail.com>
2022-06-08 09:27:04 +0800

    v2.0.6 adjust slightly

--- lib/count/count.dart
+++ lib/count/count.dart
@@ -2,12 +2,6 @@ import 'package:get/get.dart';
 
 class CountController extends GetxController {
   var count = 0;
-
-  @override
-  void onInit() {
-    debounce(count, (_) => print("debouce$_"), time: Duration(seconds: 1));//用途停止改變變數1秒後觸發
-  }
-
   void increment() {
     count++;
     update();
